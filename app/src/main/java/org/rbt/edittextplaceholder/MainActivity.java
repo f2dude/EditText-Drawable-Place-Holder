1b7479d8dfab0a47b8e6ab99abeae48719a57e78
saikrishna.pawar <saikrishna.pawar@raybiztech.com>
2016-03-04 15:23:18 +0530

    Changed the edit text to programmatically.

--- app/src/main/java/org/rbt/edittextplaceholder/MainActivity.java
+++ app/src/main/java/org/rbt/edittextplaceholder/MainActivity.java
@@ -2,19 +2,48 @@ package org.rbt.edittextplaceholder;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText et;
+    TextWatcher watcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            //The below logic is important.
+            if (!s.toString().contains("$ ")) {
+                et.setText("$ ");//Setting the text to dollar symbol with space
+                et.setSelection(et.getText().length());//Always setting the cursor position to index 2
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText et = (EditText) findViewById(R.id.edit_text);
+        et = (EditText) findViewById(R.id.edit_text);
+        et.setSelection(et.getText().length());//Always settings the cursor position to index 2
+        et.addTextChangedListener(watcher);//Interface listener for text watcher
+    }
 
-        String code = "$";
-        et.setCompoundDrawablesWithIntrinsicBounds(new TextDrawable(code), null, null, null);
-        et.setCompoundDrawablePadding(code.length() * 34); //This method is only used for drawable padding. Increase or decrease it accordingly.
+    private void getLog(String logMsg) {
+
+        Log.i("Edit Text", logMsg);
     }
 }
